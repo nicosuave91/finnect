b2ca0973a9743b81bdf8d21abb7b5b8051f7b2e2
nicosuave91 <104583391+nicosuave91@users.noreply.github.com>
2025-09-13 19:04:42 -0400

    Update AppSidebar.vue

--- frontend/components/AppSidebar.vue
+++ frontend/components/AppSidebar.vue
@@ -28,7 +28,7 @@
                 {{ tenantName }}
               </h1>
               <p class="text-xs text-gray-500 dark:text-gray-400">
-                Mortgage Platform
+                Finnect
               </p>
             </div>
           </div>
